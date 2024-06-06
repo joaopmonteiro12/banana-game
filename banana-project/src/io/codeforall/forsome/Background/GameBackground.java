dff04ee56cb3d438a4f958247e1c11a9912177ad
Joao <joaopmonteiro12@gmail.com>
2024-06-06 19:39:04 +0100

    Fixed background

--- banana-project/src/io/codeforall/forsome/Background/GameBackground.java
+++ banana-project/src/io/codeforall/forsome/Background/GameBackground.java
@@ -10,13 +10,6 @@ public class GameBackground extends Background {
     @Override
     public void createBackground() {
         super.createBackground();
-        this.background.grow(100, 100);
-        this.background.translate(100, 90);
-        //Picture vegetation = new Picture();
-        //vegetation.load("resources/vegetation.png");
-        //vegetation.draw();
-        //vegetation.grow(100, 100);
-        //vegetation.translate(100, 90);
         createGun();
 
     }
@@ -25,7 +18,7 @@ public class GameBackground extends Background {
         Picture weapon = new Picture();
         weapon.load("resources/gun.png");
         weapon.draw();
-        weapon.grow(-120,-120);
-        weapon.translate(300,398);
+        weapon.grow(-150,-150);
+        weapon.translate(190,248);
     }
 }
